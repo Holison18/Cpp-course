e468c121aaeef2f814d964b681d860a36c1a1b70
HoLySon <kobbyholison2002@gmail.com>
2023-03-07 12:29:40 +0000

    calculate sum and average added

--- arrays.cpp
+++ arrays.cpp
@@ -16,4 +16,21 @@ int main(){
         cout<<n<<" ";
     }
     cout<<"\n\n"<<endl;
+
+    // finding the sum, average and count of the array
+    int sum=0;
+    int count=0;
+    for (int i=0;i<5;i++){
+        sum+=my_array[i];
+        count++;
+    }
+    cout<<"Sum: "<<sum<<endl;
+    cout<<"Count: "<<count<<endl;
+
+    // calculate average from sum and count
+    int average = sum/count;
+    cout<<"Average: "<<average<<endl;
+    cout<<"\n\n"<<endl;
+
+
 }
